76dc12833c68cf358cda2115c8a5af6748ebb292
Bittu kumar yadav <by272215@gmail.com>
2026-06-06 17:17:42 +0530

    Add files via upload

---
+++ Predictive_Modeling_ML_Project.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+# Sample dataset
+data = {
+    "Study_Hours": [1, 2, 3, 4, 5, 6, 7, 8],
+    "Marks": [35, 40, 50, 55, 65, 70, 80, 90]
+}
+df = pd.DataFrame(data)
+X = df[["Study_Hours"]]
+y = df["Marks"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = LinearRegression()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print("R2 Score:", r2_score(y_test, predictions))
+print("MSE:", mean_squared_error(y_test, predictions))
+
+# Visualization
+plt.scatter(X, y)
+plt.plot(X, model.predict(X), linewidth=2)
+
+plt.title("Study Hours vs Marks Prediction")
+plt.xlabel("Study Hours")
+plt.ylabel("Marks")
+plt.show()
